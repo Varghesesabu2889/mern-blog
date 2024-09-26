9afeba70ceb8044be9893000b11a40b9e4e8acd3
varghesesabu2889 <varghesesabu16@gmail.com>
2024-09-26 13:18:36 +0530

    new commit

---
+++ client/src/data.js
@@ -0,0 +1,83 @@
+import Thumbnail1 from './assets/images/blog1.jpg' 
+import Thumbnail2 from './assets/images/blog2.jpg' 
+import Thumbnail3 from './assets/images/blog3.jpg' 
+import Thumbnail4 from './assets/images/blog4.jpg' 
+import Thumbnail5 from './assets/images/blog5.jpg' 
+import Thumbnail6 from './assets/images/blog6.jpg' 
+import Thumbnail7 from './assets/images/blog7.jpg' 
+import Thumbnail8 from './assets/images/blog8.jpg' 
+import Thumbnail9 from './assets/images/blog9.jpg' 
+
+export const Dummy_posts = [
+    {
+      id: '1',
+      thumbnail:Thumbnail1,
+      category:'education',
+      title: 'How to learn a new language',
+      description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique ullam architecto repellat autem mollitia necessitatibus perspiciatis, maxime blanditiis quos ad! Recusandae suscipit quae illum. Ducimus obcaecati officiis et iure distinctio! Exercitationem ab totam numquam ducimus omnis ex optio, aperiam voluptatibus deserunt tempora asperiores nemo perferendis rem suscipit autem ullam. Rerum sed perspiciatis blanditiis explicabo, totam non corrupti odit hic impedit!Possimus eaque dolor sapiente numquam non. Ipsam distinctio exercitationem eius iste sit! Earum inventore tempora sed magnam aliquam, vitae corporis cupiditate quibusdam illo dicta reprehenderit ut magni laboriosam excepturi totam?',
+      authorID:4,
+  },
+  {
+    id: '2',
+    thumbnail:Thumbnail2,
+    category:'science',
+    title: 'How to learn a new language',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique ullam architecto repellat autem mollitia necessitatibus perspiciatis, maxime blanditiis quos ad! Recusandae suscipit quae illum. Ducimus obcaecati officiis et iure distinctio! Exercitationem ab totam numquam ducimus omnis ex optio, aperiam voluptatibus deserunt tempora asperiores nemo perferendis rem suscipit autem ullam. Rerum sed perspiciatis blanditiis explicabo, totam non corrupti odit hic impedit!Possimus eaque dolor sapiente numquam non. Ipsam distinctio exercitationem eius iste sit! Earum inventore tempora sed magnam aliquam, vitae corporis cupiditate quibusdam illo dicta reprehenderit ut magni laboriosam excepturi totam?',
+    authorID:1,
+  },
+  {
+    id: '3',
+    thumbnail:Thumbnail3,
+    category:'weather',
+    title: 'How to learn a new language',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique ullam architecto repellat autem mollitia necessitatibus perspiciatis, maxime blanditiis quos ad! Recusandae suscipit quae illum. Ducimus obcaecati officiis et iure distinctio! Exercitationem ab totam numquam ducimus omnis ex optio, aperiam voluptatibus deserunt tempora asperiores nemo perferendis rem suscipit autem ullam. Rerum sed perspiciatis blanditiis explicabo, totam non corrupti odit hic impedit!Possimus eaque dolor sapiente numquam non. Ipsam distinctio exercitationem eius iste sit! Earum inventore tempora sed magnam aliquam, vitae corporis cupiditate quibusdam illo dicta reprehenderit ut magni laboriosam excepturi totam?',
+    authorID:2,
+  },
+  {
+    id: '4',
+    thumbnail:Thumbnail4,
+    category:'farming',
+    title: 'How to learn a new language',
+    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique ullam architecto repellat autem mollitia necessitatibus perspiciatis, maxime blanditiis quos ad! Recusandae suscipit quae illum. Ducimus obcaecati officiis et iure distinctio! Exercitationem ab totam numquam ducimus omnis ex optio, aperiam voluptatibus deserunt tempora asperiores nemo perferendis rem suscipit autem ullam. Rerum sed perspiciatis blanditiis explicabo, totam non corrupti odit hic impedit!Possimus eaque dolor sapiente numquam non. Ipsam distinctio exercitationem eius iste sit! Earum inventore tempora sed magnam aliquam, vitae corporis cupiditate quibusdam illo dicta reprehenderit ut magni laboriosam excepturi totam?',
+    authorID:4,
+  },{
+    id: '5',
+    thumbnail:Thumbnail5,
+    category:'weather',
+    title: 'How to learn a new language',
+      description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique ullam architecto repellat autem mollitia necessitatibus perspiciatis, maxime blanditiis quos ad! Recusandae suscipit quae illum. Ducimus obcaecati officiis et iure distinctio! Exercitationem ab totam numquam ducimus omnis ex optio, aperiam voluptatibus deserunt tempora asperiores nemo perferendis rem suscipit autem ullam. Rerum sed perspiciatis blanditiis explicabo, totam non corrupti odit hic impedit!Possimus eaque dolor sapiente numquam non. Ipsam distinctio exercitationem eius iste sit! Earum inventore tempora sed magnam aliquam, vitae corporis cupiditate quibusdam illo dicta reprehenderit ut magni laboriosam excepturi totam?',
+    authorID:2,
+  },
+  {
+    id: '6',
+    thumbnail:Thumbnail6,
+    category:'weather',
+    title: 'How to learn a new language',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor',
+    authorID:2,
+  },
+  {
+    id: '7',
+    thumbnail:Thumbnail7,
+    category:'weather',
+    title: 'How to learn javascript in the stack of mern stack ',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor',
+    authorID:2,
+  },
+  {
+    id: '8',
+    thumbnail:Thumbnail8,
+    category:'weather',
+    title: 'How to learn a new language',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor',
+    authorID:2,
+  },
+  {
+    id: '9',
+    thumbnail:Thumbnail9,
+    category:'weather',
+    title: 'How to learn a new language',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor',
+    authorID:2,
+  },
+]
--- client/src/pages/AuthorPosts.jsx
+++ client/src/pages/AuthorPosts.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PostItem from '../components/PostItem'
+import { Dummy_posts } from '../data'
 
 const AuthorPosts = () => {
+  const [posts, setPosts] = useState(Dummy_posts) 
+
   return (
-    <div>AuthorPosts</div>
+    <section className="posts">
+    {posts.length>0?
+       <div className="container posts__container">
+       {
+          posts.map(({id,thumbnail,category,title,description,authorID})=>
+            <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} description={description} authorID={authorID}/>
+          )
+        }
+       </div>: <h2 className='center'>No Post Founds</h2>
+    }
+    </section>
   )
 }
 
