3a912b322615c08a8a1ac70e36dab18efdc0020a
varghesesabu2889 <varghesesabu16@gmail.com>
2024-09-26 08:55:57 +0530

    new commit

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -3,6 +3,12 @@ import Thumbnail1 from '../assets/images/blog1.jpg'
 import Thumbnail2 from '../assets/images/blog2.jpg' 
 import Thumbnail3 from '../assets/images/blog3.jpg' 
 import Thumbnail4 from '../assets/images/blog4.jpg' 
+import Thumbnail5 from '../assets/images/blog5.jpg' 
+import Thumbnail6 from '../assets/images/blog6.jpg' 
+import Thumbnail7 from '../assets/images/blog7.jpg' 
+import Thumbnail8 from '../assets/images/blog8.jpg' 
+import Thumbnail9 from '../assets/images/blog9.jpg' 
+
 import PostItem from './PostItem'
 
 const Dummy_posts = [
@@ -37,6 +43,45 @@ const Dummy_posts = [
   title: 'How to learn a new language',
   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor',
   authorID:4,
+},{
+  id: '5',
+  thumbnail:Thumbnail5,
+  category:'weather',
+  title: 'How to learn a new language',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor',
+  authorID:2,
+},
+{
+  id: '6',
+  thumbnail:Thumbnail6,
+  category:'weather',
+  title: 'How to learn a new language',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor',
+  authorID:2,
+},
+{
+  id: '7',
+  thumbnail:Thumbnail7,
+  category:'weather',
+  title: 'How to learn a new language',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor',
+  authorID:2,
+},
+{
+  id: '8',
+  thumbnail:Thumbnail8,
+  category:'weather',
+  title: 'How to learn a new language',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor',
+  authorID:2,
+},
+{
+  id: '9',
+  thumbnail:Thumbnail9,
+  category:'weather',
+  title: 'How to learn a new language',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor',
+  authorID:2,
 },
 ]
 
